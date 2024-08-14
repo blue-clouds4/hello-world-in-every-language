c5f5ddc87d7283042612fa9d13ceadd8770b4852
blue_ <161261326+blue-clouds4@users.noreply.github.com>
2024-08-14 06:18:42 +0530

    Create helivesthenightHEISNOTANOWLISHE.vhdl

---
+++ helivesthenightHEISNOTANOWLISHE.vhdl
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity hello is
+end hello;
+architecture sim of hello is
+begin
+    process
+    begin
+        report "i do not understand that";
+        wait;
+    end process;
+end sim;
